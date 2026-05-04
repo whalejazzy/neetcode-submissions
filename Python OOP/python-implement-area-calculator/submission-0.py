1bd7e1cac9905af7d698ac4e3d72170892e0aad7
whalejazzy <156874815+whalejazzy@users.noreply.github.com>
2026-05-03 22:43:10 -0700

    Add: python-implement-area-calculator - submission-0

---
+++ Python OOP/python-implement-area-calculator/submission-0.py
@@ -0,0 +1,17 @@
+import math
+
+class AreaCalc:
+    # TODO: Implement calculate method
+    def calculate(self, length, width=None):
+        if not width:
+            return round(math.pi*(length**2), 2)
+        else:
+            return length * width
+
+    
+
+    
+# Don't modify the following code
+calc = AreaCalc()
+print(calc.calculate(5))    
+print(calc.calculate(4, 6))
