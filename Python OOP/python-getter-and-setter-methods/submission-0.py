1c54bc0e55ae0e65d2b4b0ecb7d19a376681bcc2
whalejazzy <156874815+whalejazzy@users.noreply.github.com>
2026-05-03 19:41:28 -0700

    Add: python-getter-and-setter-methods - submission-0

---
+++ Python OOP/python-getter-and-setter-methods/submission-0.py
@@ -0,0 +1,26 @@
+class BankAccount:
+    def __init__(self, balance: int):
+        self.__balance = balance
+    
+    # TODO: Add getter method for balance
+    def get_balance(self):
+        return self.__balance
+    # TODO: Add setter method for balance
+    def set_balance(self, amount):
+        if amount >= 0:
+            self.__balance = amount
+        else:
+            print("Cannot set negative balance!")
+
+
+
+
+# Don't modify the code below this line
+account = BankAccount(1000)
+print(account.get_balance())
+account.set_balance(-100)
+print(account.get_balance())
+account.set_balance(100)
+print(account.get_balance())
+account.set_balance(0)
+print(account.get_balance())
