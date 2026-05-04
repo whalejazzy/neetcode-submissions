d70f16fd5c7125b50818fcf7c38c04bcc8f706ad
whalejazzy <156874815+whalejazzy@users.noreply.github.com>
2026-05-03 22:11:05 -0700

    Add: python-method-overloading - submission-0

---
+++ Python OOP/python-method-overloading/submission-0.py
@@ -0,0 +1,14 @@
+class TextProcessor:
+    # Implement method overloading for format_text method
+    def format_text(self, text1, text2=""):
+        if not text2:
+            return text1.upper()
+        else:
+            return text1+text2
+
+
+
+# Don't modify the code below
+processor = TextProcessor()
+print(processor.format_text("hello"))
+print(processor.format_text("hello", "world"))
